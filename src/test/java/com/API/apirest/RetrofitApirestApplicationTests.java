f3e2709d348ce3542ddd6030ebb11770f6626bab
Elguedo <114593149+Elguedo@users.noreply.github.com>
2023-01-12 12:38:10 -0500

    first commit

---
+++ src/test/java/com/API/apirest/RetrofitApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
